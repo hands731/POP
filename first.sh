d13f967cc8d253171bb89606b020f3abb81ad08d
hands731 <59446995+hands731@users.noreply.github.com>
2024-10-10 13:35:05 +0900

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,13 @@
+sudo apt update -y && sudo apt upgrade -y
+echo "1"
+sudo apt install vim -y
+echo "2"
+sudo apt install net-tools -y
+echo "3"
+sudo apt-get install lightdm -y
+echo "4"
+sudo apt install openssh-server -y
+echo "5"
+sudo apt-get install -y chromium-browser unclutter xdotool
+echo "6"
+sudo reboot
